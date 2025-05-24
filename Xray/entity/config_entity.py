c6b2d62cf8ceb67edee8f01741eab6d568f66521
kmvucd <vimarish.kuttapalayammani@ucdconnect.ie>
2025-05-24 15:01:07 +0100

    Updated files for Data Ingestion

---
+++ Xray/entity/config_entity.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from dataclasses import dataclass
+from torch import device
+from Xray.constant.training_pipeline import *
+
+
+@dataclass
+
+class DataIngestionConfig:
+    def __init__(self):
+        self.S3_data_folder: str = S3_DATA_FOLDER
+        self.bucket_name: str = BUCKET_NAME
+        self.artifact: str = os.path.join(ARTIFACT_DIR, TIMESTAMP)
+        self.data_path: str=os.path.join(
+            self.artifact, "data_ingestion", 
+            self.S3_data_folder)
+        self.train_data_path: str = os.path.join(self.data_path, "train")
+        self.test_data_path: str = os.path.join(self.data_path, "test")
