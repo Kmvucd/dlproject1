48ab4d182ef942cfe255448dcaf15b4f52e46988
kmvucd <vimarish.kuttapalayammani@ucdconnect.ie>
2025-05-24 17:40:03 +0100

    Verification

---
+++ Xray/cloud_storage/s3_operation.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from Xray.exception import XrayException
+from logger import logging
+
+class S3operation:
+    
+    def sync_folder_to_s3(self, folder: str, bucket_name: str, bucket_folder_name: str)-> None: # Uploading
+        try:
+            command: str = (
+                
+                f" aws s3 sync {folder} s3://{bucket_name}/{bucket_folder_name}" 
+            )
+            
+            os.system(command)
+        except Exception as e:
+            raise XrayException(e, sys)
+        
+    def sync_folder_from_s3(self, folder: str, bucket_name: str, bucket_folder_name: str)-> None: # Downloading
+        try:
+            logging.info("Enters sync_folder_from_S3")
+            command: str = (
+                
+                f" aws s3 sync s3://{bucket_name}/{bucket_folder_name}/{folder}" 
+            )
+            os.system(command)
+        except Exception as e:
+            raise XrayException(e, sys)
